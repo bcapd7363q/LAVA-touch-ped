f8c9a4ea6df8e56a8bb6fd6b05fd99be26e7c17b
Bikash Dash <radharbikashdash@gmail.com>
2022-11-11 12:07:28 +0530

    avalable@date

--- src/components/months.vue
+++ src/components/months.vue
@@ -195,7 +195,7 @@ export default {
                       <tbody>
                         <tr class="border-b bg-red-300 border-red-500">
                           <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
-                            Primary
+                            06
                           </td>
                           <td class="text-sm text-orange-900 font-light px-6 py-4 whitespace-nowrap">
                             Cell
@@ -218,7 +218,7 @@ export default {
                         </tr>
                         <tr class="border-b bg-orange-300 border-orange-500">
                           <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
-                            Secondary
+                            07
                           </td>
                           <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                             Cell
@@ -241,7 +241,7 @@ export default {
                         </tr>
                         <tr class="border-b bg-amber-gray-300 border-amber-gray-500">
                           <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
-                            Success
+                            08
                           </td>
                           <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                             Cell
@@ -264,7 +264,7 @@ export default {
                         </tr>
                         <tr class="border-b bg-yellow-300 border-yellow-500">
                           <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
-                            Danger
+                            09
                           </td>
                           <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                             Cell
@@ -287,7 +287,7 @@ export default {
                         </tr>
                         <tr class="border-b bg-lime-300 border-lime-500">
                           <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
-                            Warning
+                            10
                           </td>
                           <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                             Cell
@@ -310,30 +310,30 @@ export default {
                         </tr>
                         <tr class="border-b bg-purple-300 border-purple-500">
                           <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
-                            Info
+                            <span v-text="date" class="rounded-full bg-red-400  px-3  ml-1 text-sm text-white-600 font-normal" />
+                          </td>
+                          <td class="text-sm text-white font-light px-6 py-4 whitespace-nowrap">
+                            04
                           </td>
                           <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                            Cell
+                            <span  class="rounded-full bg-red-400  px-3  ml-1 text-sm text-white-600 font-normal" >today</span>
                           </td>
                           <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                            Cell
+                            18
                           </td>
                           <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                            Cell
+                            25
                           </td>
-                          <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                            Cell
+                          <td class="text-sm text-white font-light px-6 py-4 whitespace-nowrap">
+                            02
                           </td>
-                          <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                            Cell
-                          </td>
-                          <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                            Cell
+                          <td class="text-sm text-white font-light px-6 py-4 whitespace-nowrap">
+                            09
                           </td>
                         </tr>
                         <tr class="border-b bg-gray-300 boder-gray-500">
                           <td class="text-sm text-dark font-medium px-6 py-4 whitespace-nowrap">
-                            Dark
+                            12
                           </td>
                           <td class="text-sm text-dark font-light px-6 py-4 whitespace-nowrap">
                             Cell
